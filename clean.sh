5b1c6f66b51694fa1a32347e69ef9849213673c2
Erdem Ersoy <erdemersoy@erdemersoy.net>
2022-02-15 15:35:14 +0300

    Remove packages_extracted via clean.sh

--- clean.sh
+++ clean.sh
@@ -16,4 +16,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-rm -rf rootfs isoimage
+rm -rf packages_extracted rootfs isoimage
